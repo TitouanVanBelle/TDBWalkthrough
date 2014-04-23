00e53ca207d332754df45c9793f8640547419a34
Titouan Van Belle <titouan.vanbelle@orange.fr>
2014-04-23 18:24:38 +0200

    XCode Project

---
+++ Example/TDBWalkthrough/TDBWalkthrough/TDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDBAppDelegate.h
+//  TDBWalkthrough
+//
+//  Created by Tito on 23/04/2014.
+//  Copyright (c) 2014 3dB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
